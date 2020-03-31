3d0010c7f2406083ab17fd52f8a30c00dc8960b3
BibianaAlcala <62916550+BibianaAlcala@users.noreply.github.com>
2020-03-31 14:29:17 -0600

    baja Marca
    baja Marca

---
+++ admin/php/bajaMarca.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    require_once 'config.php';    
+    $marcaObj = json_decode($_POST['marca']);
+    $query = "DELETE FROM marca WHERE IdMarca = $marcaObj->id";
+    $result = mysqli_query($db,$query);
+
+    if (!isset($response)) {
+        $response = new stdClass();
+    }
+
+    if($result) {
+        $response->message = 'La Marca fue eliminada exitosamente.';
+        $response->class = 'error';
+        echo json_encode($response);
+        mysqli_close($db);
+    } else {
+        $response->message = 'Error al eliminar la Marca.';
+        $response->class = 'error';
+        echo json_encode($response);
+        mysqli_close($db);
+    }

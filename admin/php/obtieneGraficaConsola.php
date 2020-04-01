2d14ff80b51a7fe0f44c6995e1dc2a8249d6bba2
EduardoValPla <aquilestra@gmail.com>
2020-04-01 17:45:33 -0600

    commit

---
+++ admin/php/obtieneGraficaConsola.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require_once 'config.php';
+    $query = "SELECT `Nombre`, COUNT(`IdConsola`) AS 'Cantidad' FROM consola GROUP BY `Nombre` ORDER BY COUNT(`IdConsola`) DESC";
+    $result = mysqli_query($db,$query);
+
+    if (!isset($response)) {
+        $response = new stdClass();
+    }
+
+    if($result) {
+        $response->data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        echo json_encode($response);
+        mysqli_close($db);
+    } else {
+        $response->message = 'Error al cargar la tabla proveedor.';
+        echo json_encode($response);
+        mysqli_close($db);
+    }

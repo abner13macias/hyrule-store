a76e1951494f5dc088cf14b7d02f63a78c8db13e
Abner Macias <abner1322.nintendo@gmail.com>
2020-04-23 14:17:38 -0500

    Agregado Registro y Carpeta PHP

---
+++ php/config.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$db_username = 'id12914556_admin';
+
+$db_password = '12345678mz';
+
+$db_name = 'id12914556_hs';
+
+$db_host = 'localhost';
+
+
+
+$db = new mysqli($db_host, $db_username, $db_password,$db_name) or die('could not connect to database');
+
+?>
---
+++ php/registrarUsuario.php
@@ -0,0 +1,25 @@
+<?php
+//Pendiente
+    session_start();
+    require_once 'config.php';
+    $employeeObj = json_decode($_POST['employee']);
+    $query = "INSERT INTO empleado (Nombre,ApellidoPaterno, ApellidoMaterno, Telefono, email, contrasenia, IdRol) VALUES ('$employeeObj->employeeName', '$employeeObj->ap_Patern',
+    '$employeeObj->ap_Matern', '$employeeObj->telefono', '$employeeObj->email', '$employeeObj->pass', '$employeeObj->idRol')";
+
+    if(mysqli_query($db, $query)) {
+        if (!isset($response)) {
+            $response = new stdClass();
+        }
+        $response->message = 'El empleado ' . $categoryObj->employeeName . ' fue dado de alta exitosamente';
+        $response->class = 'success';
+        echo json_encode($response);
+        mysqli_close($db);
+    } else {
+        if (!isset($response)) {
+            $response = new stdClass();
+        }
+        $response->message = 'Hubo un error al dar de alta al empleado, por favor intente más tarde.';
+        $response->class = 'error';
+        echo json_encode($response);
+        mysqli_close($db);
+    }

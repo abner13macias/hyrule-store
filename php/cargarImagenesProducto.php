e98d015a95d9e5b915e215d6752dbc845373eaef
Alcala12 <bibigpe@live.com.mx>
2020-04-29 16:57:06 -0500

    implementcion de imagenes a pag de producto

---
+++ php/cargarImagenesProducto.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+    require_once 'config.php';
+    $productoId = json_decode($_POST['productoId']);
+    $query = "SELECT p.Nombre, p.Precio, ip.IdArticulo, ip.IdImagen, i.Direccion 
+    FROM producto p, imagen i, imagenarticulos ip 
+    WHERE p.IdProducto = ip.IdArticulo AND i.IdImagen = ip.IdImagen and p.IdProducto=$productoId
+    UNION ALL 
+    SELECT a.Nombre, a.Precio, ip.IdArticulo, ip.IdImagen, i.Direccion 
+    FROM accesorio a, imagen i, imagenarticulos ip 
+    WHERE a.IdAccesorio = ip.IdArticulo AND i.IdImagen = ip.IdImagen and a.IdAccesorio=$productoId";
+    $result = mysqli_query($db,$query);
+
+    if (!isset($response)) {
+        $response = new stdClass();
+    }
+
+    if($result) {
+        $response->data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        echo json_encode($response);
+        mysqli_close($db);
+    } else {
+        $response->message = 'Error al cargar la tabla productos.';
+        echo json_encode($response);
+        mysqli_close($db);
+    }

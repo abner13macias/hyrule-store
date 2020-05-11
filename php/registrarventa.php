21ddee067abc9d66c7a30796f053a417f6264c97
EduardoValPla <aquilestra@gmail.com>
2020-05-10 22:55:33 -0500

    commit

---
+++ php/registrarventa.php
@@ -0,0 +1,24 @@
+<?php
+//Pendiente
+    session_start();
+    require_once 'config.php';
+    $userObj = json_decode($_POST['carrito']);
+    $hashedPass = password_hash($userObj->pass, PASSWORD_DEFAULT);
+    $query = "INSERT INTO `venta`(`Total`, `Status`, `Fecha`, `Id_Carrito`) VALUES ('$userObj->total','$userObj->status','Finalizado','$userObj->idcarrito')";
+
+    
+    if (!isset($response)) {
+        $response = new stdClass();
+    }
+
+    if(mysqli_query($db, $query)) {
+        //$response->message = 'Fuiste dado de alta exitosamente!';
+        $response->class = 'success';
+        echo json_encode($response);
+        mysqli_close($db);
+    } else {
+        //$response->message = 'Hubo un error, intenta más tarde.';
+        $response->class = 'error';
+        echo json_encode($response);
+        mysqli_close($db);
+    }
